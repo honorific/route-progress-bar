8ca35f24c000b951931525d6c95bdd1d978c0fe0
honorific <honorific.info@gmail.com>
2023-05-17 10:21:04 +0330

    implement routing in app directory

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 import Link from 'next/link'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
     <>
-      
+      <Link href='/about' className={styles.menu}>
+        About
+      </Link>
+      <Link href='/contact' className={styles.menu}>
+        Contact
+      </Link>
       <div>Home page</div>
     </>
   )
